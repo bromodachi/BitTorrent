2ad44482bb830ab7d4b26be7487386a0d930baa6
bromodachi <curaga1@gmail.com>
2014-06-28 13:53:50 -0400

    Most info extracted. However, don't know what to do with the SHA-1. Will create a Torrent Info object or make this a static or final class so we can use the info later on. Have to do: 1. Check for error checking. 2. Figure out what to do with SHA-1 3. Be sure we extracted everything we needed. For now, this is for sharing what I have atm.

--- BitTorrentClient/src/btClient/main.java
+++ BitTorrentClient/src/btClient/main.java
@@ -7,6 +7,8 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.io.*;
 import java.net.*; //not sure if we need this one yet but let's see
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 import java.util.*;
 
 public class main {
@@ -18,8 +20,11 @@ public class main {
 	public static void main(String[] args) throws IOException, BencodingException {
 		File torrentFile;
 		byte [] torrentBytes;
-		Map<ByteBuffer, Object> metaInfo; //for extracting the url and info
-											//but can't seem to get the info
+		byte [] SHA1_hash;
+		Map<ByteBuffer, Object> torrentMap; //bencoder gives us a Map for the url
+											//use this to get the url and the info map
+		Map<ByteBuffer, Object> torrentInfo; //the torrent Info's map. Use this to get the torrent
+											//infromation
 		// TODO Auto-generated method stub
 		//blah blah blha this is a comment
 		//When compiled, we only allow two args so:
@@ -34,23 +39,58 @@ public class main {
 			 */
 			//check 2nd arg...
 			if(fileExtensionChecker2ndArg(args[1])){
+				String path="";
 				System.out.println("Success");
+				//Create a file with the first arg, then get its bytes
 				torrentFile=new File(args[0]);
 				torrentBytes=getFileBytes(torrentFile);
-				System.out.println(torrentBytes);
-				//will change the name
-				Bencoder2 test=new Bencoder2();
-				//decode returns a map, in the description
-				metaInfo=(Map<ByteBuffer, Object>)test.decode(torrentBytes);
-				ByteBuffer url_bytes=(ByteBuffer) metaInfo.get(ByteBuffer.wrap(new byte[]{'a', 'n','n','o', 'u','n','c','e'}));
-		//this seems to do nothing:		
-			//	ByteBuffer info_bytes=(ByteBuffer) metaInfo.get(ByteBuffer.wrap(new byte[]{'i', 'n','f','o'}));
-			//	String info=new String(info_bytes.array(), "ASCII");
-			//	System.out.println(info);
+				//Extract the maps from the torrent's bytes
+				torrentMap=(Map<ByteBuffer, Object>)Bencoder2.decode(torrentBytes);
+				ByteBuffer url_bytes=(ByteBuffer) torrentMap.get(ByteBuffer.wrap(new byte[]{'a', 'n','n','o', 'u','n','c','e'}));
+				torrentInfo=(Map<ByteBuffer, Object>) torrentMap.get(ByteBuffer.wrap(new byte[]{'i', 'n','f','o'}));
+				//	Extracting the info keys
+				// Should check if we get a length or a file(latter if for 
+				// multiple files. Will we be downloading multiple files?
+				ByteBuffer info_bytes=Bencoder2.getInfoBytes(torrentBytes);
+				ByteBuffer name_bytes=(ByteBuffer) torrentInfo.get(ByteBuffer.wrap(new byte[]{'n', 'a','m','e'}));
+				ByteBuffer pieces_bytes=(ByteBuffer) torrentInfo.get(ByteBuffer.wrap(new byte[]{'p', 'i','e','c','e','s'}));
+				ByteBuffer path_bytes=(ByteBuffer) torrentInfo.get(ByteBuffer.wrap(new byte[]{'p', 'a','t','h'}));
+				/*The lengths:*/
+				int infoLength=(int) torrentInfo.get(ByteBuffer.wrap(new byte[]{'l', 'e','n','g','t','h'}));
+				int piecesLength=(int) torrentInfo.get(ByteBuffer.wrap(new byte[]{'p', 'i','e','c','e',' ','l', 'e','n','g','t','h'}));
 				
+				/*Converting some of the bytes into string. However,
+				 * some seems to be crypted. */
+				String fileName=new String(name_bytes.array(), "ASCII");
 				String urlAddress=new String(url_bytes.array(), "ASCII");
+				String pieces=new String(pieces_bytes.array(), "ASCII");
+				String info_test=new String(info_bytes.array(), "ASCII");
+				if(path_bytes!=null){
+					System.out.println("yes");
+					path=new String(path_bytes.array(), "ACII");
+				}
+				try {
+					/*don't know what to do with this*/
+						MessageDigest crypt=MessageDigest.getInstance("SHA-1");
+						crypt.update(info_bytes);
+					} catch (NoSuchAlgorithmException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+				if(pieces.length()%20!=0){
+					System.out.println("Error, not a multiple of 20");
+					return;
+				}
 				
 				System.out.println(urlAddress);
+				System.out.println(fileName);
+				System.out.println(infoLength);
+				System.out.println(piecesLength);
+				System.out.println(pieces.length());
+				System.out.println("Rest is gibberish:");
+				System.out.println(pieces);
+				System.out.println(path);
+				System.out.println("testing info: "+info_test);
 		
 			}
 			else{
@@ -81,7 +121,7 @@ public class main {
 	}
 	
 	/**
-	 * Will check if a torrent file is valid. if not, returns a boolean.
+	 * Will check if a torrent file is valid. returns a boolean.
 	 * @param torrentFile
 	 * @return true or false
 	 */
